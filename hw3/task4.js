adbd91832149798baced1a68fd47b734dc6100af
Igor <kpe4ets@gmail.com>
2020-08-06 21:23:50 +0300

    hw3 update

--- hw3/task4.js
+++ hw3/task4.js
@@ -1,17 +1,1 @@
-//4) *Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
-//x
-//xx
-//xxx
-//xxxx
-//xxxxx
-
-
-function drawTriangle(length, drawerBase) {
-    var drawer = '';
-    for (var i=1; i<=length; i++) {
-        drawer += drawerBase;
-        console.log(drawer);
-    }
-}
-
-drawTriangle(20, 'x');
+for (var i=0; i<10; console.log(i++));
